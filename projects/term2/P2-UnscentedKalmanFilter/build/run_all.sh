dfa34f424c026ba6f4594857c87d681a293314b3
Mohan Karthik <nemesis.nitt@gmail.com>
2017-03-31 22:03:04 +0530

    Adding the output files for reference

---
+++ projects/term2/P2-UnscentedKalmanFilter/build/run_all.sh
@@ -0,0 +1,8 @@
+./UnscentedKF ../data/sample-laser-radar-measurement-data-1.txt out_both_1.txt true true true > dbg_both_1.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-1.txt out_lidar_1.txt false true true > dbg_lidar_1.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-1.txt out_radar_1.txt true false true > dbg_radar_1.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-1.txt out_none_1.txt false false true > dbg_none_1.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-2.txt out_both_2.txt true true true > dbg_both_2.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-2.txt out_lidar_2.txt false true true > dbg_lidar_2.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-2.txt out_radar_2.txt true false true > dbg_radar_2.txt
+./UnscentedKF ../data/sample-laser-radar-measurement-data-2.txt out_none_2.txt false false true > dbg_none_2.txt
