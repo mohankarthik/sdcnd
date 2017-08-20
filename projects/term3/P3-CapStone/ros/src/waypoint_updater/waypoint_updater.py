c8906b0a26cef8734cf797bb73be50cbbe4b57d9
Mohan Karthik <nemesis.nitt@gmail.com>
2017-08-21 05:18:41 +0530

    Implemented a simple waypoint updater to follow the lane at a fixed speed

--- projects/term3/P3-CapStone/ros/src/waypoint_updater/waypoint_updater.py
+++ projects/term3/P3-CapStone/ros/src/waypoint_updater/waypoint_updater.py
@@ -26,26 +26,64 @@ LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this n
 
 class WaypointUpdater(object):
     def __init__(self):
+        # Initialize the node
         rospy.init_node('waypoint_updater')
 
+        # Initialize the incoming topics
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
+        # Initialize the outgoing final waypoints topic
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
+        # Other variables
+        self.mapX = []  # variable to store waypoint X coordinate from /base_waypoints
+        self.mapY = []  # variable to store waypoint Y coordinate from /base_waypoints
+        self.map_wp_len = 0  # numper of map waypoints in /base_waypoints
+        self.mapWP = []  # array to store map waypoints for full information
 
+        # Spin the node
         rospy.spin()
 
     def pose_cb(self, msg):
-        # TODO: Implement
+        print 'got pose info'
+        pose_x = msg.pose.position.x
+        pose_y = msg.pose.position.y
+        nearest_waypoint = self.closest_waypoint(pose_x, pose_y, self.mapX, self.mapY)
+        # TODO: can return closest waypoint even if it is behind, need to add orientation check
+
+        # Get next LOOKAHEAD_WPS waypoints
+        pub_list = []
+        for i in range(LOOKAHEAD_WPS):
+            pub_list.append(self.mapWP[nearest_waypoint + i])
+
+        # Create correct data structure for publishing
+        lane = Lane()
+        lane.header.frame_id = msg.header.frame_id
+        lane.header.stamp = rospy.Time(0)
+        lane.waypoints = pub_list
+
+        # Publish to /final_waypoints
+        if not rospy.is_shutdown():
+            self.final_waypoints_pub.publish(lane)
         pass
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
+        # Create two lists one with X and other with Y waypoints
+        new_mapX = []
+        new_mapY = []
+        new_mapWP = []
+        for waypoint in waypoints.waypoints[:]:
+            new_mapX.append(waypoint.pose.pose.position.x)
+            new_mapY.append(waypoint.pose.pose.position.y)
+            new_mapWP.append(waypoint)
+
+        # Assign to global variables
+        self.mapX = new_mapX
+        self.mapY = new_mapY
+        self.mapWP = new_mapWP
+        self.map_wp_len = len(waypoints.waypoints)
         pass
 
     def traffic_cb(self, msg):
@@ -69,6 +107,24 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+
+    # Calculate distance between two points
+    def dist_two_points(self, x1, y1, x2, y2):
+        return math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+
+    # Finds nearest waypoint
+    def closest_waypoint(self, x, y, maps_x, maps_y):
+        closest_wp_dist = 999999.9
+        closest_wp = 0
+
+        for i in range(self.map_wp_len):
+            map_x = maps_x[i]
+            map_y = maps_y[i]
+            dist = self.dist_two_points(x, y, map_x, map_y)
+            if (dist < closest_wp_dist):
+                closest_wp_dist = dist
+                closest_wp = i
+        return closest_wp
 
 
 if __name__ == '__main__':
